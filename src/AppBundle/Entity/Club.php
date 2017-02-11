52417342b3277cba62a3984c92b8936a6316820b
YrenWu <clementine-tournery@hotmail.fr>
2017-02-11 13:29:21 +0100

    enable yml routing

---
+++ src/AppBundle/Entity/Club.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Club
+ *
+ * @ORM\Table(name="club")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ClubRepository")
+ */
+class Club
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="history", type="text", nullable=true)
+     */
+    private $history;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="text", nullable=true)
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set history
+     *
+     * @param string $history
+     *
+     * @return Club
+     */
+    public function setHistory($history)
+    {
+        $this->history = $history;
+
+        return $this;
+    }
+
+    /**
+     * Get history
+     *
+     * @return string
+     */
+    public function getHistory()
+    {
+        return $this->history;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return Club
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
